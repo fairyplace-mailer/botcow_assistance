8140dcaa9a49ece807d6e9f296992406fa27fdc9
fairyplace-mailer <fairyplace.tm@gmail.com>
2025-12-12 13:17:11 +0200

    test: map @octokit/rest to mock for jest

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testPathIgnorePatterns: ['/node_modules/', '/.next/']
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+  moduleNameMapper: {
+    '^@octokit/rest$': '<rootDir>/tests/__mocks__/octokitRest.ts'
+  }
 };

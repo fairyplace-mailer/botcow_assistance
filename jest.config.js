fbc4b2de59230e2f9082d28f181096f886709c36
fairyplace-mailer <fairyplace.tm@gmail.com>
2026-04-03 17:23:11 +0300

    test: remove duplicate jest config

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
-  moduleNameMapper: {
-    '^@octokit/rest$': '<rootDir>/tests/__mocks__/octokitRest.ts'
-  },
-  setupFiles: ['<rootDir>/tests/setupEnv.ts']
-};

8262ee2c4256f84698cf85cb414ea27433c8bacc
fairyplace-mailer <fairyplace.tm@gmail.com>
2026-04-04 14:50:32 +0300

    test: run both src and top-level tests

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,9 +1,9 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  roots: ['<rootDir>/src'],
+  roots: ['<rootDir>/src', '<rootDir>/tests'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
-  testMatch: ['**/*.test.ts'],
+  testMatch: ['**/*.test.ts', '**/*.spec.ts'],
   clearMocks: true,
   restoreMocks: true,
   resetMocks: true,
